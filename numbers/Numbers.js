71e907c6ab5941355997a1e0f7840b426d554e56
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-05-26 19:26:39 +0530

    Numbers in JS

---
+++ numbers/Numbers.js
@@ -0,0 +1,16 @@
+var num1=5;
+num1++; //Increment 
+num1--; // Decrement
+console.log(num1);
+
+console.log(10%3); // remainder
+
+console.log(2*5);
+console.log(8/4);
+
+// Increment,Decrement by any num
+ var num=10;
+ num+=10;
+ console.log(num);
+
+ 
