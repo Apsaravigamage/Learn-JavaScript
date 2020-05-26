14e2470baf8e9e502a3f261056cb483a24015776
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-05-26 22:15:01 +0530

    DataTypes in JS

---
+++ DataTypes/datatype.js
@@ -0,0 +1,31 @@
+var yourAge=20;                       //number
+var yourName='Janu';                  //string
+var name={first:'Janu',Last:'Domi'};  //object
+var truth=false;                      //boolean
+var fruit =['apple','banana','mango'];//array
+var random;                           //undefined
+var nothing=null ;                    //value null
+
+console.log(yourAge);
+console.log(yourName);
+console.log(name);
+console.log(truth);
+console.log(fruit);
+console.log(random);
+console.log(nothing);
+
+// Strings in JS (common methods)
+let fruits ='banana';
+let morefruit = 'banana\napple'; // newline
+console.log(morefruit);
+
+console.log(fruits.length);
+console.log(fruits.indexOf('q'));
+console.log(fruits.slice(2,6)); // includ in and up to
+console.log(fruits.replace('ban','123'));
+console.log(fruits.toUpperCase(fruits));
+console.log(fruits.toLowerCase(fruits));
+console.log(fruits.charAt(2));
+console.log(fruits[2]);
+console.log(fruits.split('')); // split by a charactes
+console.log(fruits.split('')); // split by a comma
