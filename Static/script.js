e203f567bf61a7799f227434e77bfd4771afdbfc
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-05-28 20:08:26 +0530

    First Challenge(Your age in Days) - step 2

--- Static/script.js
+++ Static/script.js
@@ -1,11 +1,14 @@
 //challenge 1:Your Age in Day
 function ageInDays() {
     var birthYear = prompt('What age were you brom....?');
-    var ageInDayss =(2018-birthYear)*365;
+    var ageInDayss =(2020-birthYear)*365;
     var h1=document.createElement('h1');
     var textAnswer = document.createTextNode('You are'+ageInDayss +'Days old');
     h1.setAttribute('id','ageInDays');
     h1.appendChild(textAnswer);
     document.getElementById('flex-box-result').appendChild(h1);
- 
+}
+function reset(){
+    document.getElementById('ageInDays').remove();
+    
 }
